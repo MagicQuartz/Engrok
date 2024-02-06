e70ffad6678b83b18a2aa54acf2b374c7613ad1a
Kesem Hershcovich <kesem100k@gmail.com>
2024-02-06 21:07:26 +0200

    Idk

---
+++ src/main/java/io/github/magicquartz/engrok/EngrokServer.java
@@ -0,0 +1,10 @@
+package io.github.magicquartz.engrok;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+
+public class EngrokServer implements DedicatedServerModInitializer {
+    @Override
+    public void onInitializeServer() {
+
+    }
+}
